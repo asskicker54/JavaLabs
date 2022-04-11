8d4d5f24b7ac678879a12a8e51eb833dbeb1c5f8
asskicker54 <jnreey547@gmail.com>
2022-04-11 10:21:09 +0700

    init

---
+++ GUI/swap/src/viewmodel/ViewModel.java
@@ -0,0 +1,32 @@
+package viewmodel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import model.Model;
+
+public class ViewModel {
+    @FXML
+    TextField t1;
+
+    @FXML
+    TextField t2;
+
+    @FXML
+    Button button;
+
+    private boolean swapped = false;
+
+    public void onClick() {
+        String[] text = Model.swap(t1.getText(), t2.getText());
+        t1.setText(text[0]);
+        t2.setText(text[1]);
+
+        if (swapped) {
+            button.setText(">");
+        } else {
+            button.setText("<");
+        }
+        swapped = !swapped;
+    }
+}
